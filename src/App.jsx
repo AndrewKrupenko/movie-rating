89fdd7464b5b89f214cf372f70798920ce0a1769
Andrew Krupenko <andrewkrupenko@Andrews-MacBook-Pro.local>
2025-12-28 23:59:57 +0100

    created the 'Add Movie' form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import { ALL_MOVIES } from "./data/movies";
 import MovieItem from "./components/MovieItem";
+import Modal from "./components/ui/Modal";
+import MovieForm from "./components/MovieForm";
 
 export default function App() {
   const movies = ALL_MOVIES.items;
+  const showMovieForm = true;
+  const currentMovie = null;
 
   return (
     <div className="app">
+      <Modal
+        isOpen={showMovieForm}
+        title={currentMovie?.id ? "Edit Movie" : "Add Movie"}
+      >
+        <MovieForm
+          movie={currentMovie}
+          onSave={(data) => {
+            console.log("Movie saved:", data);
+          }}
+          onCancel={() => {
+            console.log("Movie form cancelled");
+          }}
+        />
+      </Modal>
       <div className="movie-list">
         {movies.map((movie) => {
           return <MovieItem key={movie.id} movie={movie} />;
